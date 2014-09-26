700d93543ee29219aa970b718caa5b389054eedc
Andrew McCluskey <andrew@ajmccluskey.com>
2014-09-26 14:03:19 -0500

    Remove incorrect comment from convert-hex-to-b64
    First attempt at solution took code from an SO answer, but our final
    solution was not a copy of this answer - hence the comment removal.

--- src/matasano_clj/helpers.clj
+++ src/matasano_clj/helpers.clj
@@ -2,7 +2,7 @@
   (:require [clojure.data.codec.base64 :as b64]))
 
 (defn convert-hex-to-b64 [hex]
-  "Set1 - Challenge 1. Code taken from http://stackoverflow.com/a/16781372/510722"
+  "Set1 - Challenge 1."
   (let [char-partitions (partition 2 2 (repeat \0) hex)
         byte-sized-ints (map #(Integer/parseInt (reduce str %) 16) char-partitions)]
     (String. (b64/encode (byte-array (map byte byte-sized-ints))))))
