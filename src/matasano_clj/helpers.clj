0e0664e38f204b2ffe616c8a91e2c2ffd35b6e46
Andrew McCluskey <andrew@ajmccluskey.com>
2014-09-26 17:11:30 -0500

    Extract functionality for hex string to byte seq
    Our next challenge also takes a hex string and needs to work on bytes,
    so extract this functionality to its own function for reuse.

--- src/matasano_clj/helpers.clj
+++ src/matasano_clj/helpers.clj
@@ -1,11 +1,15 @@
 (ns matasano-clj.helpers
   (:require [clojure.data.codec.base64 :as b64]))
 
-(defn convert-hex-to-b64 [hex]
+(defn hex-string-to-bytes [s]
+  "Convert a character string to the seq of bytes it represents. String
+  must be in hex and contain an even number of characters."
+  (assert (even? (count s)))
+  (let [char-pairs (partition 2 s)
+        int-pairs (map #(Integer/parseInt (reduce str %) 16) char-pairs)]
+    (map byte int-pairs)))
+
+(defn convert-hex-to-b64 [s]
   "Set1 - Challenge 1. Converts a string of hexadecimal digits to a base64
   encoded string. hex must have an even length"
-  (assert (even? (count hex)))
-
-  (let [char-partitions (partition 2 hex)
-        byte-sized-ints (map #(Integer/parseInt (reduce str %) 16) char-partitions)]
-    (String. (b64/encode (byte-array (map byte byte-sized-ints))))))
+  (String. (b64/encode (byte-array (hex-string-to-bytes s)))))
