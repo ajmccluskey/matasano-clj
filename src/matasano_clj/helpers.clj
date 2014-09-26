b5972ac5b590a75a5e91852d976c5fe6f55979dc
Andrew McCluskey <andrew@ajmccluskey.com>
2014-09-26 17:26:27 -0500

    Add solution to challenge 2
    Add function to bitwise XOR the sequences of bits represented by the 2
    given strings of hex. Output is a string of hex.

--- src/matasano_clj/helpers.clj
+++ src/matasano_clj/helpers.clj
@@ -9,7 +9,19 @@
         int-pairs (map #(Integer/parseInt (reduce str %) 16) char-pairs)]
     (map byte int-pairs)))
 
+(defn bytes-to-hex-string [coll]
+  "Output a string of hex characters representing the given collection of bytes."
+  (reduce str (map #(format "%x" %) coll)))
+
 (defn convert-hex-to-b64 [s]
   "Set1 - Challenge 1. Converts a string of hexadecimal digits to a base64
   encoded string. hex must have an even length"
   (String. (b64/encode (byte-array (hex-string-to-bytes s)))))
+
+(defn fixed-xor [a b]
+  "Set 1 - Challenge 2. Bitwise xor 2 equal length buffers."
+  (assert (= (count a) (count b)))
+  (let [bytes-a (hex-string-to-bytes a)
+        bytes-b (hex-string-to-bytes b)
+        xored-bytes (map bit-xor bytes-a bytes-b)]
+    (bytes-to-hex-string xored-bytes)))
