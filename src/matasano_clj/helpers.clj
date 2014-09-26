06a5a30313e3777b301bbfd762807706a6d4d818
Andrew McCluskey <andrew@ajmccluskey.com>
2014-09-26 14:13:29 -0500

    Simplify partitioning in convert-hex-to-64
    Padding our hex string with 0s was incorrect - just assert that the hex
    string has an even number of characters.

--- src/matasano_clj/helpers.clj
+++ src/matasano_clj/helpers.clj
@@ -2,7 +2,10 @@
   (:require [clojure.data.codec.base64 :as b64]))
 
 (defn convert-hex-to-b64 [hex]
-  "Set1 - Challenge 1."
-  (let [char-partitions (partition 2 2 (repeat \0) hex)
+  "Set1 - Challenge 1. Converts a string of hexadecimal digits to a base64
+  encoded string. hex must have an even length"
+  (assert (even? (count hex)))
+
+  (let [char-partitions (partition 2 hex)
         byte-sized-ints (map #(Integer/parseInt (reduce str %) 16) char-partitions)]
     (String. (b64/encode (byte-array (map byte byte-sized-ints))))))
