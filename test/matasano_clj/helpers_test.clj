0e4f28d32d271cd90ca257866ae767e8898a9786
Andrew McCluskey <andrew@ajmccluskey.com>
2014-09-26 17:23:45 -0500

    Add test for challenge 2
    Also add "-challenge" suffix on test names to avoid collisions with
    functions under test.

--- test/matasano_clj/helpers_test.clj
+++ test/matasano_clj/helpers_test.clj
@@ -2,7 +2,13 @@
   (:require [clojure.test :refer :all]
             [matasano-clj.helpers :refer :all]))
 
-(deftest hex-to-base64
+(deftest hex-to-base64-challenge
   (testing "Hex string from challenge 1 converts correctly"
     (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
            (convert-hex-to-b64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")))))
+
+(deftest fixed-xor-challenge
+  (testing "XOR 2 hex strings of equal length")
+  (is (= "746865206b696420646f6e277420706c6179"
+         (fixed-xor "1c0111001f010100061a024b53535009181c"
+                    "686974207468652062756c6c277320657965"))))
